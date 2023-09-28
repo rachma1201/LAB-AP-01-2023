10af26ec6853f996d80b3eaf3c45062eeedddaad
Ramma <72251408+rachma1201@users.noreply.github.com>
2023-09-28 10:52:51 +0800

    Update TP2_2_H071231027.py

--- H071231027/Praktikum-2/TP2_2_H071231027.py
+++ H071231027/Praktikum-2/TP2_2_H071231027.py
@@ -1,17 +1,6 @@
 x = input("Masukkan Input Pertama : ")
-if x != "vertebrado" and "invertebrado":
-    print("invalid input")
-    exit()
-    
 y = input("Masukkan Input Kedua : ")
-if y != "ave" and "mamifero" and "inseto" and "anelido":
-    print("invalid input")
-    exit()
-    
 z = input("Masukkan Input Ketiga : ")
-if z != "carnivoro" and "onivoro"and "herbivoro" and "hematofoga":
-    print("invalid input")
-    exit()
 
 match x, y, z:
     case "vertebrado", "ave", "carnivoro":
