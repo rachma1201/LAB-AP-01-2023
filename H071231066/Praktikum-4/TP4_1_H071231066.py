aa0804a2463d2f8106b8db04474fa765c1126a21
ReyRiz <farizidhamr@gmail.com>
2023-10-05 13:49:53 +0700

    a

--- H071231066/Praktikum-4/TP4_1_H071231066.py
+++
@@ -1,8 +0,0 @@
-def geserhuruf(n): 
-    for i in range(len(n)):
-        n = n[1:]+ n[0]
-        print(n, end=" | ")
-try:
-    geserhuruf("#@!")
-except TypeError:
-    print("Terjadi error, N harus bertipe data string")
